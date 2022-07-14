a3c9785c26fc270c80677987d0be37a9574c1dc2
Dario Balač <dario.balac@transcom.com>
2022-07-14 15:21:56 +0200

    Created table and filled it with data from API

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from './../service/api.service';
-
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
@@ -8,22 +11,53 @@ import { ApiService } from './../service/api.service';
 })
 export class CoinListComponent implements OnInit {
 
-  constructor(private api: ApiService) { }
+bannerData: any=[];
+dataSource!: MatTableDataSource<any>;
+currency : string = "EUR"
+displayedColumns: string[] = ['symbol', 'current_price', 'price_change_percentage_24h', 'market_cap'];
+
+@ViewChild(MatPaginator) paginator!: MatPaginator;
+@ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private api: ApiService, private router : Router) { }
 
   ngOnInit(): void {
     this.getAllData()
     this.getBannerData()
   }
+
+
   getBannerData() {
     this.api.getTrendingCurrency("EUR")
       .subscribe(res => {
         console.log(res);
+        this.bannerData = res;
       })
   }
+
+
   getAllData() {
     this.api.getCurrency("USD")
       .subscribe(res => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
         console.log(res);
       })
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  gotoDetails(row: any) {
+    this.router.navigate(['coin-detail',row.id])
   }
 }
